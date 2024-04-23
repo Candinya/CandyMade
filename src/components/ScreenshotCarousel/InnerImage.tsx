380a2a8e20d7eeb549e053fcbd07c803a05f50a9
Nya Candy <dev@candinya.com>
2024-04-23 17:40:20 +0800

    feat: click to open image

--- src/components/ScreenshotCarousel/InnerImage.tsx
+++ src/components/ScreenshotCarousel/InnerImage.tsx
@@ -6,13 +6,15 @@ interface InnerImageProps {
 }
 const InnerImage = ({ screenshot, isActive }: InnerImageProps) => (
   <div
-    className={`transition-opacity duration-700 ease-in-out ${isActive ? "opacity-100" : "opacity-0"}`}
+    className={`transition-opacity duration-700 ease-in-out ${isActive ? "opacity-100" : "opacity-0 pointer-events-none"}`}
   >
-    <img
-      src={screenshot.imageUrl}
-      className="absolute block h-full w-full object-contain -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-      alt={screenshot.title}
-    />
+    <a href={screenshot.imageUrl} target="_blank">
+      <img
+        src={screenshot.imageUrl}
+        className="absolute block max-h-full max-w-full object-contain -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        alt={screenshot.title}
+      />
+    </a>
   </div>
 );
 
