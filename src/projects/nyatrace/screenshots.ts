6f5f1d36c606628b596aa86c82571b8f0e206067
Nya Candy <dev@candinya.com>
2024-04-27 18:31:01 +0800

    feat: update screenshot for nyatrace

--- src/projects/nyatrace/screenshots.ts
+++ src/projects/nyatrace/screenshots.ts
@@ -3,6 +3,22 @@ import type { ProjectScreenShot } from "@/types/project";
 export const ScreenShot: ProjectScreenShot[] = [
   {
     imageUrl: "/assets/screenshots/nyatrace/main.png",
-    title: "运行界面",
+    title: "主要界面",
+  },
+  {
+    imageUrl: "/assets/screenshots/nyatrace/map.png",
+    title: "地图显示追踪路径与可信范围",
+  },
+  {
+    imageUrl: "/assets/screenshots/nyatrace/logs.png",
+    title: "等级控制的日志粒度",
+  },
+  {
+    imageUrl: "/assets/screenshots/nyatrace/settings.png",
+    title: "分模块设置管理",
+  },
+  {
+    imageUrl: "/assets/screenshots/nyatrace/about.png",
+    title: "关于 NyaTrace",
   },
 ];
