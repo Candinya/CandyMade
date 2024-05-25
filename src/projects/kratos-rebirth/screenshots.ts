651a92371e56c51b70ca21e7ce19a3fd1e4e9512
Nya Candy <dev@candinya.com>
2024-05-25 16:17:14 +0800

    feat: add all-platforms screenshot for Kratos : Rebirth

--- src/projects/kratos-rebirth/screenshots.ts
+++ src/projects/kratos-rebirth/screenshots.ts
@@ -1,3 +1,8 @@
 import type { ProjectScreenShot } from "@/types/project";
 
-export const ScreenShots: ProjectScreenShot[] = [];
+export const ScreenShots: ProjectScreenShot[] = [
+  {
+    imageUrl: "/assets/screenshots/kratos-rebirth/all-platforms.png",
+    title: "全平台响应式概览",
+  },
+];
