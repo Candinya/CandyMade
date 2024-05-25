e9f943ebc6013fe3cd2fb80ba65e81cb6acea101
Nya Candy <dev@candinya.com>
2024-05-25 16:18:08 +0800

    fix: missing space

--- src/projects/kratos-rebirth/basic.ts
+++ src/projects/kratos-rebirth/basic.ts
@@ -2,7 +2,7 @@ import type { ProjectBasicInfo } from "@/types/project";
 
 export const Basic: ProjectBasicInfo = {
   id: "kratos-rebirth",
-  name: "Kratos: Rebirth",
+  name: "Kratos : Rebirth",
   slogan: "一个可爱而现代的 Hexo 主题",
   logo: "/assets/logos/kratos-rebirth.png",
   sourceRepo: "https://github.com/Candinya/Kratos-Rebirth",
