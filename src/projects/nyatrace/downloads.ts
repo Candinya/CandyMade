604c9339caee43280b0269172f1cb6def63d7205
Nya Candy <dev@candinya.com>
2025-02-15 23:36:47 +0800

    chore: remove china optimize downloads

--- src/projects/nyatrace/downloads.ts
+++ src/projects/nyatrace/downloads.ts
@@ -5,8 +5,4 @@ export const Downloads: ProjectDownload[] = [
     url: "https://github.com/Candinya/NyaTrace/releases/latest",
     text: "从原始发布下载",
   },
-  {
-    url: "https://nyadisk.net/s/AqFx",
-    text: "中国大陆加速下载",
-  },
 ];
