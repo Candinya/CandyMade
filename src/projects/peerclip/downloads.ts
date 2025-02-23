4abbab6b6fd1e3429e3a5528b53bf3917e83e6ef
Nya Candy <dev@candinya.com>
2025-02-23 21:51:12 +0800

    feat: add download link for peerclip

--- src/projects/peerclip/downloads.ts
+++ src/projects/peerclip/downloads.ts
@@ -1,3 +1,8 @@
 import type { ProjectDownload } from "@/types/project";
 
-export const Downloads: ProjectDownload[] = [];
+export const Downloads: ProjectDownload[] = [
+  {
+    url: "https://github.com/Candinya/PeerClip/releases/latest",
+    text: "从原始发布下载",
+  },
+];
