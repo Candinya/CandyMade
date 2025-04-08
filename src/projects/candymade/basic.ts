3e482a8cf1f951dbe79ee877b63072278059ac1e
Nya Candy <dev@candinya.com>
2025-04-08 09:34:50 +0800

    chore: update license in project info

--- src/projects/candymade/basic.ts
+++ src/projects/candymade/basic.ts
@@ -6,7 +6,7 @@ export const Basic: ProjectBasicInfo = {
   slogan: "简单，清晰，干净的项目展示页",
   logo: "/assets/logos/candymade.svg",
   sourceRepo: "https://github.com/Candinya/CandyMade",
-  license: "MIT (改)",
+  license: "Apache 2.0",
   startFrom: new Date("2024-04-22"),
   type: "Web",
   isPublished: true,
